30025602b0d182537c1f61e722a8699115425853
antin antin <annadominie@gmail.com>
2020-09-26 11:42:02 +0300

    finish filtering

--- src/org/github/onetwostory/CarChoose.java
+++ src/org/github/onetwostory/CarChoose.java
@@ -1,5 +1,6 @@
 package org.github.onetwostory;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,19 +23,37 @@ public class CarChoose {
     }
 
     public List<Car> chooseByModelAndExpluatation(String model, int yearsOfExpluatation) {
-        cars
+        final List<Car> byModelAndYear = cars
                 .stream()
                 .filter(car -> isModel(car, model))
-                .filter(car -> {
-                    car.getYearOfCreation()
-                })
+                .filter(car -> checkForAge(car, yearsOfExpluatation))
+                .collect(Collectors.toList());
+        return byModelAndYear;
+    }
+
+    public List<Car> chooseByYearAndPrice(int year, int price) {
+        final List<Car> byYearAndPrice = cars
+                .stream()
+                .filter(car -> checkforEqualsAge(car, year))
+                .filter(car -> checkForBiggerPrice(car, price))
+                .collect(Collectors.toList());
+        return byYearAndPrice;
     }
 
     // Private
 
+    private boolean checkForBiggerPrice(Car car, int price) {
+        return car.getPrice() > price;
+    }
+
+    private boolean checkforEqualsAge(Car car, int year) {
+        final int yearNow = new GregorianCalendar().get(Calendar.YEAR);
+        return car.getYearOfCreation() == yearNow;
+    }
+
     private boolean checkForAge(Car car, int yearsOfExpluatation) {
-        new GregorianCalendar()
-        return (car.getYearOfCreation()) >
+        final int yearNow = new GregorianCalendar().get(Calendar.YEAR);
+        return ((car.getYearOfCreation() + yearsOfExpluatation) < yearNow);
     }
 
     private boolean isModel(Car car, String model) {
