bebc85103f4083f625bb4526640eb42b12757849
bataisk <donnatroal@gmail.com>
2020-09-26 12:09:42 +0300

    Add main and factory

--- src/org/github/onetwostory/Main.java
+++ src/org/github/onetwostory/Main.java
@@ -1,9 +1,15 @@
 package org.github.onetwostory;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        CarChoose cars = new CarChoose(CarsFactory.createCarsList());
 
+        System.out.println(cars.chooseByCompany("Deo"));
+        System.out.println(cars.chooseByModelAndExpluatation("ML-240", 3));
+        System.out.println(cars.chooseByYearAndPrice(2000, 20000));
 
     }
 }
