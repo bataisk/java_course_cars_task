f0c7f833e0d79190157256d224d8d42a5e3a5817
bataisk <donnatroal@gmail.com>
2020-10-03 12:05:34 +0300

    add tests

--- src/test/java/org/github/onetwostory/CarChooseTest.java
+++ src/test/java/org/github/onetwostory/CarChooseTest.java
@@ -1,39 +1,39 @@
-//package org.github.onetwostory;
-//
-//import org.junit.Assert;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.*;
-//
-//public class CarChooseTest {
-//
-//    private CarChoose cars;
-//
-//    @org.junit.Before
-//    public void setUp() throws Exception {
-//        cars = new CarChoose(CarsFactory.createCarsList());
-//    }
-//
-//    @org.junit.Test
-//    public void chooseByCompany() {
-//        final List<Car> deo = cars.chooseByCompany("Deo");
-//        deo.stream()
-//                .forEach(carTest -> {
-//                    final String company = carTest.getCompany().toUpperCase();
-//                    Assert.assertEquals(company, "DEO");
-//                });
-//
-//    }
-//
-//    @org.junit.Test
-//    public void chooseByModelAndExpluatation() {
-//        final List<Car> cars = this.cars.chooseByModelAndExpluatation("ML-240", 3);
-//
-//
-//    }
-//
-//    @org.junit.Test
-//    public void chooseByYearAndPrice() {
-//    }
-//}
+package org.github.onetwostory;
+
+import org.junit.Assert;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CarChooseTest {
+
+    private CarChoose cars;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        cars = new CarChoose(CarsFactory.createCarsList());
+    }
+
+    @org.junit.Test
+    public void chooseByCompany() {
+        final List<Car> nissan = cars.chooseByCompany("Nissan");
+        nissan.stream()
+                .forEach(carTest -> {
+                    final String company = carTest.getCompany();
+                    Assert.assertEquals(company, "Nissan");
+                });
+    }
+
+    @org.junit.Test
+    public void chooseByModelAndExpluatation() {
+        final List<Car> cars = this.cars.chooseByModelAndExpluatation("GT-R", 1);
+        Assert.assertEquals(9871263L, cars.get(0).getId());
+    }
+
+    @org.junit.Test
+    public void chooseByYearAndPrice() {
+        final List<Car> cars = this.cars.chooseByYearAndPrice(2002, 20000);
+        Assert.assertEquals(2, cars.size());
+    }
+}
