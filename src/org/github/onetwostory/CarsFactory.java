e627b19bc820fb2d1a026c0589050ec453f5f290
bataisk <donnatroal@gmail.com>
2020-10-03 09:20:32 +0300

    Add some cars to factory

--- src/org/github/onetwostory/CarsFactory.java
+++ src/org/github/onetwostory/CarsFactory.java
@@ -5,12 +5,22 @@ import java.util.*;
 public class CarsFactory {
     public static List<Car> createCarsList() {
         List<Car> result = new ArrayList<>();
-        result.add(new Car(1414124, "Mersedes", "ML-240", "Purple", 12000, "AM231", 2000));
-        result.add(new Car(8765433, "Deo", "Lanos", "White", 2000, "AM121", 2015));
-        result.add(new Car(1231314, "Tayota", "Prado", "Black", 60000, "AM931", 2018));
-        result.add(new Car(7868672, "Mersedes", "AM-130", "Purple", 24000, "AM321", 2010));
-        result.add(new Car(6546546, "Deo", "Matis", "Red", 3000, "AM231", 2011));
-        result.add(new Car(2352342, "Mersedes", "ML-240", "Black", 12000, "AM342", 2019));
+        result.add(new Car(1414124, "Nissan", "SENTRA", 2004, "Purple",
+                12000, "L-934U"));
+        result.add(new Car(9871263, "Nissan", "GT-R", 2018, "White",
+                210000, "L-154P"));
+        result.add(new Car(2876269, "Nissan", "Maxima", 2019, "Black",
+                70000, "L-752H"));
+        result.add(new Car(8765433, "Sterling Truck", "CONDOR", 2002, "White",
+                8000, "L-4758"));
+        result.add(new Car(1231314, "Hyosung", "GT250R", 2002, "Black",
+                60000, "L-588f"));
+        result.add(new Car(7868672, "Arctic Cat", "M 8000 ROB KINCAID SE", 2002, "Purple",
+                45000, "L-548c"));
+        result.add(new Car(6546546, "BMW", "428I XDRIVE", 2002, "Red",
+                90000, "L-919M"));
+        result.add(new Car(2352342, "Kawasaki", "JH1100 ULTRA 130", 2002, "Black",
+                16000, "L-871A"));
 
         return result;
     }
--- src/org/github/onetwostory/Main.java
+++ src/org/github/onetwostory/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         CarChoose cars = new CarChoose(CarsFactory.createCarsList());
 
-        System.out.println(cars.chooseByCompany("Deo"));
-        System.out.println(cars.chooseByModelAndExpluatation("ML-240", 3));
-        System.out.println(cars.chooseByYearAndPrice(2000, 20000));
+        System.out.println(cars.chooseByCompany("Nissan"));
+        System.out.println(cars.chooseByModelAndExpluatation("GT-R", 1));
+        System.out.println(cars.chooseByYearAndPrice(2002, 20000));
 
     }
 }
